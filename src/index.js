f34b4d5dde645f825e53d098a3a0f71cd82c354c
Matt Powell <mpowell171@gmail.com>
2022-03-08 12:33:36 -0500

    cleaned up src directory and moved troubleshooting scripts out of the way Co-authored-by: @aidynhillyard @JAndrzGutierrez

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './troubleshooting/reportWebVitals';
 
 
 ReactDOM.render(
