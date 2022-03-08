c9de22a7f8f41b10ebdd1d678f4c8c832a8a7071
Matt Powell <mpowell171@gmail.com>
2022-03-08 18:48:18 -0500

    added axios to dependencies, GET request in GroupsPage functioning Co-authored-by: @aidynhillyard

--- src/Components/GroupsPage/GroupsPage.js
+++ src/Components/GroupsPage/GroupsPage.js
@@ -1,6 +1,18 @@
 import GroupForm from "../GroupForm/GroupForm";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import apiUrl from "../../apiUrl";
 
 const GroupsPage = () => {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(async () => {
+    const response = await axios.get(apiUrl + "/groups");
+    setGroups(response.data.groups);
+  }, []);
+
+  console.log(groups);
+
   return (
     <div>
       <h2>I'm a GroupsPage</h2>
