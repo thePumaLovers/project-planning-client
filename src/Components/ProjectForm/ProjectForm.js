e79ce4c2b396373241c121b9248114e3204e9ec0
rosscarnegie <97925198+rosscarnegie@users.noreply.github.com>
2022-03-08 17:28:07 -0500

    modified project form and project page

--- src/Components/ProjectForm/ProjectForm.js
+++ src/Components/ProjectForm/ProjectForm.js
@@ -1,7 +1,50 @@
-const ProjectForm = () => {
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+
+function ProjectForm() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
-      <h2>I'm a ProjectForm</h2>
+      <Button variant="primary" onClick={handleShow}>
+        Launch static backdrop modal
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Project Name</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Project Description</Form.Label>
+              <Form.Control type="email" placeholder="Enter description" />
+            </Form.Group>  <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Completed?</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
--- src/Components/ProjectsPage/ProjectsPage.js
+++ src/Components/ProjectsPage/ProjectsPage.js
@@ -1,8 +1,12 @@
+import ProjectForm from "../ProjectForm/ProjectForm";
+
 const ProjectsPage = () => {
   return (
     <div>
       <h2>I'm a ProjectsPage</h2>
+      <ProjectForm />
     </div>
+
   );
 };
 
