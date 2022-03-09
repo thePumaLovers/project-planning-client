3ed362e9a9e8bcef23ed1e16a31df4e481b98211
Matt Powell <mpowell171@gmail.com>
2022-03-08 22:29:50 -0500

    added bootstrap nav, removed Nav Component Co-authored-by: @aidynhillyard

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,27 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+
 const Header = () => {
   return (
     <div>
-      <h2>I'm a Header</h2>
+      <Navbar bg="light" expand="lg">
+  <Container>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Brand href="#home">Project Planning</Navbar.Brand>
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Groups</Nav.Link>
+        <Nav.Link href="#link">Projects</Nav.Link>
+        <Nav.Link href="#link">Team</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+    
+
+  </Container>
+</Navbar>
     </div>
   );
 };
--- src/Components/Nav/Nav.js
+++
@@ -1,9 +0,0 @@
-const Nav = () => {
-  return (
-    <div>
-      <h2>I'm a Nav</h2>
-    </div>
-  );
-};
-
-export default Nav;
