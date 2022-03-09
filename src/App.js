52887a0f24017cfc3118d45c6656b0356f302f51
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-03-09 18:52:26 -0500

    Configured the footer to render always at the foot of the page.Co-authored-by: @aidynhillyard @mpowell773

--- src/App.js
+++ src/App.js
@@ -5,29 +5,34 @@ import ProjectsPage from "./Components/ProjectsPage/ProjectsPage";
 import ProjectPage from "./Components/ProjectPage/ProjectPage";
 import TeamPage from "./Components/TeamPage/TeamPage";
 import Footer from "./Components/Footer/Footer";
-
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <Container id="page-container" className="App">
+      <Container id="content-wrap" >
+        <Header />
 
-      <main>
-        <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
-          <Route path="/groups" element={<GroupsPage />}></Route>
-          <Route path="/projects" element={<ProjectsPage />}></Route>
-          <Route path="/projects/:projectid" element={<ProjectPage />}></Route>
-          <Route path="/team" element={<TeamPage />}></Route>
-        </Routes>
-      </main>
-
-      <Footer />
-    </div>
-
-   
+        <main>
+          <Routes>
+            <Route path="/" element={<LandingPage />}></Route>
+            <Route path="/groups" element={<GroupsPage />}></Route>
+            <Route path="/projects" element={<ProjectsPage />}></Route>
+            <Route
+              path="/projects/:projectid"
+              element={<ProjectPage />}
+            ></Route>
+            <Route path="/team" element={<TeamPage />}></Route>
+          </Routes>
+        </main>
+      </Container>
+      <Container id="footer">
+        <Footer/>
+      </Container>
+      
+    </Container>
   );
 }
 
