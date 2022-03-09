ad71d927c04c5da10bc19e970782f91397620fec
Matt Powell <mpowell171@gmail.com>
2022-03-08 19:22:44 -0500

    added DELETE request via button for GroupsPage Co-authored-by: @aidynhillyard

--- src/Components/GroupsPage/GroupsPage.js
+++ src/Components/GroupsPage/GroupsPage.js
@@ -2,30 +2,33 @@ import GroupForm from "../GroupForm/GroupForm";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import apiUrl from "../../apiUrl";
+import Button from 'react-bootstrap/Button'
 
 const GroupsPage = () => {
   const [groups, setGroups] = useState([]);
   const [toggle, setToggle] = useState(true)
-  // const [newGroup, setNewGroup] = useState({
-  //   displayName: "",
-  //   location: "",
-  //   projects: [],
-  // });
-
+  
   // GET request for all groups
   useEffect(async () => {
     const response = await axios.get(apiUrl + "/groups");
     setGroups(response.data.groups);
   }, [toggle]);
 
+  // Mapping our groups to render on page
   const listGroups = groups.map((group, index) => {
     return (
       <div key={index}>
         <h3>{group.displayName}</h3>
         <p>{group.location}</p>
+        <Button variant="danger" onClick={() => handleDelete(group._id)}>Delete</Button>
         </div>
     )
   })
+
+  const handleDelete = (id) => {
+    axios.delete(apiUrl + '/groups/' + id )
+    setToggle(!toggle)
+  }
 
 console.log(groups)
   return (
