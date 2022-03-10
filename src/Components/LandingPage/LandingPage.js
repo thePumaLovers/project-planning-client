83383c560755d72cd3a981396d304c33a80d3419
Aidyn Hillyard <aidyn@Aidyns-MacBook-Pro.local>
2022-03-09 21:05:56 -0600

    began adjusting footer background color

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <Container className="landing-page">
       <h1>Project Planning</h1>
-      <p>Welcome to Project Planning, a way to organize your projects.  Whether you want to build a birdhouse or plan a cross-country road trip, Project Planning will help you explore your goals and break them down into manageable steps!</p>
+      <p>Welcome to Project Planning, a way to organize your projects.  Whether you want to build a birdhouse or plan a cross-country road trip, Project Planning will help you explore your goals and help you break them down into manageable steps!</p>
     </Container>
   );
 };
