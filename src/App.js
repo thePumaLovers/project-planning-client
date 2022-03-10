eab7ed76b9b0650545afea3bf9324334276ee760
Matt Powell <mpowell171@gmail.com>
2022-03-09 22:52:59 -0500

    deleted singular project page due to new direction of project Co-authored-by: @aidynhillyard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from "./Components/Header/Header";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import GroupsPage from "./Components/GroupsPage/GroupsPage";
 import ProjectsPage from "./Components/ProjectsPage/ProjectsPage";
-import ProjectPage from "./Components/ProjectPage/ProjectPage";
 import TeamPage from "./Components/TeamPage/TeamPage";
 import Footer from "./Components/Footer/Footer";
 import { Routes, Route } from "react-router-dom";
@@ -12,7 +11,7 @@ import Container from "react-bootstrap/Container";
 function App() {
   return (
     <Container id="page-container" className="App">
-      <Container id="content-wrap" >
+      <Container id="content-wrap">
         <Header />
 
         <main>
@@ -20,18 +19,13 @@ function App() {
             <Route path="/" element={<LandingPage />}></Route>
             <Route path="/groups" element={<GroupsPage />}></Route>
             <Route path="/projects" element={<ProjectsPage />}></Route>
-            <Route
-              path="/projects/:projectid"
-              element={<ProjectPage />}
-            ></Route>
             <Route path="/team" element={<TeamPage />}></Route>
           </Routes>
         </main>
       </Container>
       <Container id="footer">
-        <Footer/>
+        <Footer />
       </Container>
-      
     </Container>
   );
 }
