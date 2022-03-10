7ce1b2319935b10281bf8c5117597673a6afe91c
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-03-09 19:31:27 -0500

    Added color palettes variables Co-authored-by: @aidynhillyard

--- src/Components/TeamPage/TeamPage.js
+++ src/Components/TeamPage/TeamPage.js
@@ -24,10 +24,9 @@ const TeamPage = () => {
         <Card style={{ width: "18rem" }}>
           <Card.Img variant="top" src="https://picsum.photos/id/1/286/180" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Juan Andres Gutierrez</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            Hi! I’m Juan Andres, I am originally from Costa Rica. As a Software Engineer I look strive to use my talent, knowledge and experience to create positive impact in the world so I collaborated with other Super Talented Engineers and decided to create an App that makes you life easier and more organized. Things that I enjoy the most is spending time with my wifey. Train my Pups, a Fancy Dinner, a Crisp Cold Coke, Marvel Movies, I can’t forget Coffee and nice Lazy Sunday of just doing nothing but to exist.
             </Card.Text>
           </Card.Body>
         </Card>
