2765854399852e106e5adf5c8c28677712b14351
rosscarnegie <97925198+rosscarnegie@users.noreply.github.com>
2022-03-10 17:12:07 -0500

    Added video to landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -5,19 +5,20 @@ import backgroundVideo from './videofile.mp4'
 const LandingPage = () => {
   return (
     <Container className="landing-page">
-      <div className="Main Animation Div">
-      <video autoPlay muted id="video">
+      <div className="mainAnimatedDiv">
+      <video 
+      autoPlay 
+      muted id="video"
+      style={{
+        position: "relative",
+        width: "100%",
+        left: 0,
+        top: 0}}>
       <source src={backgroundVideo} type='video/mp4'></source>  
       </video>
-      <div className="animatedDiv div1">Think it</div>
-      <div className="animatedDiv div2">Dream it</div>
-      <div className="animatedDiv div3">Plan it</div>
-      <div className="animatedDiv div4">Do it</div>
-      <div className="animatedDiv div5">Done: With project planner</div>
-      </div>
-      
       <h1>Project Planning</h1>
       <p>Welcome to Project Planning, a way to organize your projects.  Whether you want to build a birdhouse or plan a cross-country road trip, Project Planning will help you explore your goals and help you break them down into manageable steps!</p>
+      </div>
     </Container>
   );
 };
